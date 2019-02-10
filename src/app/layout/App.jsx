0447862ab9e0734237e12a7c68247f1e1b0f0011
rades12340 <rades12340@gmail.com>
2019-02-11 00:00:53 +0100

    inital commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import moment from "moment";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
