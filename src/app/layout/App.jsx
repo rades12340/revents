77ad3ed9bbaf5cd1ad017c0243879d41bc1fd780
rades12340 <rades12340@gmail.com>
2019-02-13 19:37:22 +0100

    after  adding react router

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,41 @@
 /* eslint-disable jsx-a11y/href-no-hash */
 import React, { Component } from "react";
-import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import EventDetailedPage from "../../features/events/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventForm from "../../features/events/EventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar className="main" />
-        <Container>
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar className="main" />
+              <Container>
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
---
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PeopleDashboard = () => {
+  return (
+    <div>
+      <h1>PeopleDashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum impedit
+        repellat architecto, officiis quae placeat voluptate cupiditate commodi
+        error, assumenda veniam beatae deserunt? Incidunt, excepturi deserunt
+        exercitationem iure cumque odit!
+      </p>
+    </div>
+  );
+};
+
+export default PeopleDashboard;
