63c06792c018bb3f65a6d57449f8ac8b82ca53a5
rades12340 <rades12340@gmail.com>
2019-02-20 00:14:59 +0100

    auth reducer added and finished

--- src/app/store/reducers/rootReducer.js
+++ src/app/store/reducers/rootReducer.js
@@ -3,7 +3,7 @@ import testReducer from "../../../features/testarea/testReducer";
 import eventReducer from "../../../features/events/eventReducer";
 import { reducer as FormReducer } from "redux-form";
 import modalReducer from "../../../features/modals/modalReducer";
-import { authReducer } from "../../../features/auth/authReducer";
+import authReducer from "../../../features/auth/authReducer";
 
 const rootReducer = combineReducers({
   form: FormReducer,
