1a75607205fcd4913e25fde0e75b9622ce2abce6
rades12340 <rades12340@gmail.com>
2019-03-06 19:05:30 +0100

    comments and subcomments added

--- src/features/events/eventActions.jsx
+++ src/features/events/eventActions.jsx
@@ -1,6 +1,6 @@
 import { toastr } from "react-redux-toastr";
 import moment from "moment";
-import { DELETE_EVENT, FETCH_EVENTS } from "./eventConstants";
+import { FETCH_EVENTS } from "./eventConstants";
 import {
   asyncActionStart,
   asyncActionFinish,
@@ -115,5 +115,29 @@ export const getEventsForDashboard = lastEvent => async (
   } catch (error) {
     console.log(error);
     dispatch(asyncActionError());
+  }
+};
+
+export const addEventComment = (eventId, values, parentId) => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+  const profile = getState().firebase.profile;
+  const user = firebase.auth().currentUser;
+  let newComment = {
+    parentId: parentId,
+    displayName: profile.displayName,
+    photoURL: profile.photoURL || "/public/assets/user.png",
+    uid: user.uid,
+    text: values.comment,
+    date: Date.now()
+  };
+  try {
+    await firebase.push(`event_chat/${eventId}`, newComment);
+  } catch (error) {
+    console.log(error);
+    toastr.error("Oops", "Problem adding comment");
   }
 };
